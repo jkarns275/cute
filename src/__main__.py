141f776185d343a733e7de8b88b0fbc9e9aa5f9e
Joshua Karns <jkarns275@gmail.com>
2020-06-25 15:31:16 -0400

    allow tensorflow outputs

--- src/__main__.py
+++ src/__main__.py
@@ -3,7 +3,7 @@ import sys
 
 import os
 # This hides tensorflow debug output
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'  # or any {'0', '1', '2'}
 
 from mpi4py import MPI
 
